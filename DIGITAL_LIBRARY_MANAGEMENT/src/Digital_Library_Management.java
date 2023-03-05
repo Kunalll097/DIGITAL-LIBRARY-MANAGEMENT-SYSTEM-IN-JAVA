8311530608a62c28fb2114c68cb3541f4e1a00dd
Kunal Ramchandra Lade <113702259+Kunalll097@users.noreply.github.com>
2023-03-05 14:44:06 +0530

    Add files via upload
    ------------Features of a digital library-----------
    -No Physical Restrictions.
    -Multiple Access.
    -Easy-to-Use.
    -Conservation and Preservation.
    -No Limitation of Space.
    -Scope of Improvement.
    -More Than a Library.
    -Not Time-Bound.

---
+++ DIGITAL_LIBRARY_MANAGEMENT/src/Digital_Library_Management.java
@@ -0,0 +1,18 @@
+//KUNAL LADE (Oasis Infobyte) OIB/M1/IP5783
+
+public class Digital_Library_Management {
+
+
+    public static void main(String[] args)
+    {
+
+
+//        System.out.println("\n");
+//        System.out.println("KUNAL LADE (Oasis Infobyte) OIB/M1/IP5783\n");
+//        System.out.println("-----------------Welcome to the Digital Library!-----------------");
+//        System.out.println(" Select From The Following Options : ");
+//        System.out.println("-----------------------------------------------------------------");
+//        System.out.println("\n");
+
+    }
+}
